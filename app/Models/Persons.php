f1e653ecd06a826f4715643a8a9317a836f41d6f
Jasonjason10 <ofil0001@hz.nl>
2022-02-10 14:31:57 +0100

    update

--- app/Models/Persons.php
+++ app/Models/Persons.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class persons extends Model
+class Persons extends Model
 {
     use HasFactory;
 }
