cd9803f161fff101ede427ec03baf222af1859bc
Jasonjason10 <ofil0001@hz.nl>
2022-02-14 11:42:24 +0100

    added the grades table

---
+++ database/migrations/2022_02_14_103632_create_grades_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateGradesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('grades', function (Blueprint $table) {
+            $table->id();
+            $table->string('course_name');
+            $table->string('test_name');
+            $table->decimal('lowest_passing_grade');
+            $table->decimal('best_grade');
+            $table->date('passed_at');
+            $table->timestamps('created_at');
+            $table->timestamps('updated_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('grades');
+    }
+}
