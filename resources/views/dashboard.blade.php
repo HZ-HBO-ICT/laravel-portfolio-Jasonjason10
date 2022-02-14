b5ce0cbe5c1748a892397be3ed95dd42c4a199f1
Jasonjason10 <ofil0001@hz.nl>
2022-02-14 14:15:57 +0100

    made a temple and tried to improve link

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,6 +1,5 @@
-<!DOCTYPE html>
-<html>
-
+@extends('layout')
+@section('head-content')
 <head>
     <title>Dashboard</title>
     <link rel="stylesheet" href="style.css">
@@ -57,47 +56,9 @@
 
     </style>
 </head>
+@endsection
 
-<body>
-
-<div id="mySidenav" class="sidenav">
-    <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
-    <img src="https://vmh-hbo.nl/wp-content/uploads/2018/06/hz-logo.jpg" alt="HZ logo" width="180" height="150">
-    <a href="https://hz.nl/over-de-hz/regelingen-documenten-1/onderwijs-en-examenregelingen" target="_blank"
-       rel="noopener noreferrer">Onderwijs-en-examenregelingen</a>
-    <a href="https://apps.hz.nl/angular/studievoortgang/studiestatus" target="_blank"
-       rel="noopener noreferrer">Studievoortgang</a>
-    <a href="https://learn.hz.nl/" target="_blank" rel="noopener noreferrer">Learn HZ</a>
-    <a href="https://hz.nl/uploads/documents/Regelingen/OERS/2019-2020/2020-2021-ICT-Implementation-Regulations-CER-HZ-DEF1.0.pdf"
-       target="_blank" rel="noopener noreferrer">Uitvoersregeling</a>
-    <a href="https://github.com/" target="_blank" rel="noopener noreferrer">Github</a>
-    <a href="https://teams.microsoft.com/l/team/19%3a827654897ab746089c081f24aff1c984%40thread.skype/conversations?groupId=337e8cca-f67d-4132-9fa9-b0c761bbeb94&tenantId=4c16deb3-342d-4fca-bcd5-b1429308034c"
-       target="_blank" rel="noopener noreferrer">Teams HBO-ICT</a>
-
-</div>
-
-
-<span style="font-size:30px;cursor:pointer" onclick="openNav()">&#9776; Menu</span>
-
-<script>
-    function openNav() {
-        document.getElementById("mySidenav").style.display = "block";
-    }
-
-    function closeNav() {
-        document.getElementById("mySidenav").style.display = "none";
-    }
-</script>
-
-<nav>
-    <ul>
-        <li><a href="/"> Home</a></li>
-        <li><a href="profile">Profile</a></li>
-        <li><a href="FAQ">FAQ</a></li>
-        <li><a href="dashboard">Dashboard</a></li>
-        <li><a href="blog">Blog</a></li>
-    </ul>
-</nav>
+@section('content')
 
 <main>
 
@@ -202,7 +163,4 @@
 
 
 </main>
-</body>
-
-
-</html>
+@endsection
--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -130,25 +130,74 @@
 {{--        </div>--}}
 {{--    </body>--}}
 {{--</html>--}}
+    <!DOCTYPE html>
+<html>
 
-@extends('layout')
-@section('content')
-<main>
-    <title>Home</title>
-    <h1>Jason Ofili</h1>
-    <img src="img.jpg/jason.jpg" alt="assassins creed" width="317" height="394">
-    <img src="https://www.1337.games/app/uploads/2020/12/PS5-Games-Graphic.jpg" alt="gamen" width="450" height="394">
+<head>
+@yield('head-content')
+    <link rel="stylesheet" href="style.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <style>
+        header {
+            color: white;
+        }
 
-    <h1>Inhoud</h1>
+
+        ul {
+
+            color: white;
+        }
+
+    </style>
+</head>
+
+<body>
+
+<div id="mySidenav" class="sidenav">
+    <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
+    <img src="https://vmh-hbo.nl/wp-content/uploads/2018/06/hz-logo.jpg" alt="HZ logo" width="180" height="150">
+    <a href="https://hz.nl/over-de-hz/regelingen-documenten-1/onderwijs-en-examenregelingen" target="_blank"
+       rel="noopener noreferrer">Onderwijs-en-examenregelingen</a>
+    <a href="https://apps.hz.nl/angular/studievoortgang/studiestatus" target="_blank"
+       rel="noopener noreferrer">Studievoortgang</a>
+    <a href="https://learn.hz.nl/" target="_blank" rel="noopener noreferrer">Learn HZ</a>
+    <a href="https://hz.nl/uploads/documents/Regelingen/OERS/2019-2020/2020-2021-ICT-Implementation-Regulations-CER-HZ-DEF1.0.pdf"
+       target="_blank" rel="noopener noreferrer">Uitvoersregeling</a>
+    <a href="https://github.com/" target="_blank" rel="noopener noreferrer">Github</a>
+    <a href="https://teams.microsoft.com/l/team/19%3a827654897ab746089c081f24aff1c984%40thread.skype/conversations?groupId=337e8cca-f67d-4132-9fa9-b0c761bbeb94&tenantId=4c16deb3-342d-4fca-bcd5-b1429308034c"
+       target="_blank" rel="noopener noreferrer">Teams HBO-ICT</a>
+
+</div>
+
+
+<span style="font-size:30px;cursor:pointer" onclick="openNav()">&#9776; Menu</span>
+
+<script>
+    function openNav() {
+        document.getElementById("mySidenav").style.display = "block";
+    }
+
+    function closeNav() {
+        document.getElementById("mySidenav").style.display = "none";
+    }
+</script>
+
+<nav>
     <ul>
-        <li>Profiel</li>
-        <li>Mijn blog</li>
-        <li>FAQ</li>
-        <li>Dashboard</li>
+
+        <li class="{{Request::path()=== '/' ? 'current_page_item' : ''}}"><a href="/"> Home</a></li>
+        <li><a href="profile">Profile</a></li>
+        <li><a href="FAQ">FAQ</a></li>
+        <li><a href="dashboard">Dashboard</a></li>
+        <li><a href="blog">Blog</a></li>
     </ul>
-    <p>Mijn naam is Jason Ofili. Ik ben een 17 jaar en een ICT student van Hogeschool Zeeland.</p>
-    <p>Ik heb voor ICT gekozen omdat ik altijd al geïntresseerd ben in computers en technologie en ik wil daar nu
-        meer over leren en daar later een baan in vinden. ICT speelt in het dagelijks leven een steeds grotere rol en er zijn heel veel mogelijkheden in. Ik ben atijd als goed geweest in het werken met computers en ik kan snel nieuwe concepten leren en begrijpen. En ik ben een harde werker die niet snel opgeeft. Met Programeren zelf heb ik geen ervaring maar ik wil het graag leren. </p>
+</nav>
+
+<main>
+    @yield('content')
 
 </main>
-@endsection
+</body>
+</html>
+
