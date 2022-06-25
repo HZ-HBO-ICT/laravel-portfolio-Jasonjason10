0e7884ffca9be6af9787983036e1ceb0e75942f0
Jasonjason10 <ofil0001@hz.nl>
2022-06-25 20:05:37 +0200

    added costum error message

--- resources/lang/en/auth.php
+++ resources/lang/en/auth.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'failed' => 'This information is incorrect please try again.',
+    // 'failed' => 'This information is incorrect please try again.',
     'password' => 'The provided password is incorrect.',
     'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
 
