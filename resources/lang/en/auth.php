b066e556f38c9903867ec4c28785e9d8d6ebd683
Jasonjason10 <ofil0001@hz.nl>
2022-06-26 13:39:37 +0200

    added custom login

--- resources/lang/en/auth.php
+++ resources/lang/en/auth.php
@@ -13,8 +13,18 @@ return [
     |
     */
 
-    'failed' => 'This information is incorrect please try again.',
-    'custom',
+    'failed' => [
+        'password' => [
+            'required' => 'you need to fill in a password',
+            'password' => 'this password is incorrect'
+        ],
+        'email'=>[
+            'required' => 'you need to fill in a email',
+            'email' => 'this email is invalid'
+
+        ]
+    ],
+    'custom'=> '',
     'password' => 'The provided password is incorrect.',
     'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
 
