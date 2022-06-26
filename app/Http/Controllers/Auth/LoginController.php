528df8a5c4947cf6b9b2776aa938ad652b037467
Jasonjason10 <ofil0001@hz.nl>
2022-06-26 14:15:18 +0200

    added custom login

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -49,7 +49,6 @@ class LoginController extends Controller
     protected function validator(array $message)
     {
         return Validator::make($message, [
-            'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
             'password' => ['required', 'string', 'min:8', 'confirmed', 'password'],
         ]);
