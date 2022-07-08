ff4945c72af5e97b606c9909172daea48b63a40f
Jasonjason10 <ofil0001@hz.nl>
2022-07-08 22:02:48 +0200

    added error preventie

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -2,12 +2,7 @@
 
 @section('content')
 <div class="container">
-    <div class="explanation-register">
-    <ul>
-        <li>Your password needs to have a least 8 characters</li>
-        <li>You can't pick the same email twice</li>
-    </ul>
-    </div>
+
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
@@ -79,6 +74,12 @@
                 </div>
             </div>
         </div>
+    </div>
+    <div class="explanation-register">
+    <ul>
+        <li>Your password needs to have a least 8 characters</li>
+        <li>You can't pick the same email twice</li>
+    </ul>
     </div>
 </div>
 @endsection
