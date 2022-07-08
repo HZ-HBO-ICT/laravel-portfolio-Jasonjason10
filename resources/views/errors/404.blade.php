7b994cbaf0207f5acf10686529a3c8abafecda0a
Jasonjason10 <ofil0001@hz.nl>
2022-07-08 22:59:34 +0200

    added error pages

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>404 Custom Error Page </title>
+    <title>404 Error Page </title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
 </head>
 <body>
