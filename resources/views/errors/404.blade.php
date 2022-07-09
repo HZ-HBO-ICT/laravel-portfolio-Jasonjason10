e5e62517e4bfcedb2a078cc26b5145da33140124
Jasonjason10 <ofil0001@hz.nl>
2022-07-09 15:02:30 +0200

    added error pages

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,30 +1,45 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>404 Error Page </title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
     <style>
-        button{
+        .button {
             text-align: center;
             justify-content: center;
             position: relative;
         }
+
         a {
             color: black;
         }
+
+        .center {
+            text-align: center;
+        }
+
+        .button {
+            position: absolute;
+            top: 50%;
+        }
     </style>
 </head>
+
 <body>
     <div class="container mt-5 pt-5">
         <div class="alert alert-danger text-center">
             <h2 class="display-3">404</h2>
             <p class="display-5">Oops! Something is wrong please go back!</p>
         </div>
-        <button onclick="history.back()">Go Back</button>
-        <button><a href="/">Home</a></button>
+        <div class="center">
+            <button class="button" onclick="history.back()">Go Back</button>
+            <button class="button"><a href="/">Home</a></button>
+        </div>
     </div>
 
 </body>
+
 </html>
